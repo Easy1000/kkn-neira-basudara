7c5f0d9e7b77d5db0a5c4c1b8e1f78e2afdbdec2
Gelora Manalu <gelora.dam2003@mail.ugm.ac.id>
2024-06-02 18:12:35 +0700

    Card: added mobile responsiveness

--- src/app/components/Card/Card.jsx
+++ src/app/components/Card/Card.jsx
@@ -1,22 +1,20 @@
 import React from 'react'
 import Image from 'next/image'
 
-const Card = ({imagePath, title, subtitle}) => {
+const Card = ({imagePath, title, subtitle, className}) => {
   return (
     <div>
-        <div className='w-full h-[500px] backdrop-blur-md rounded-[30px] overflow-clip border-[1px] drop-shadow-lg flex flex-col bg-white/10 hover:scale-105 transition duration-150 ease-out hover:ease-in hover:cursor-pointer'>
-            
-            <div className='bg-gray-700 w-full h-[317px] overflow-clip'>
-                <Image src={imagePath}  className='h-auto w-full lg:-translate-y-10' alt='card image' />
+        <div className={`w-full lg:h-[500px] h-auto backdrop-blur-md rounded-[30px] overflow-clip border-[1px] drop-shadow-lg flex flex-col bg-white/10 hover:scale-105 transition duration-150 ease-out hover:ease-in hover:cursor-pointer ${className}`}>
+            <div className='bg-gray-700 w-full h-auto overflow-clip'>
+                <Image src={imagePath}  className='h-auto w-full ' alt='card image' />
             </div>
-            <div className='p-12 '>
-                <h2 className='text-c-green font-manjari lg:text-2xl font-extrabold hover:brightness-125 transition duration-150 ease-out hover:ease-in hover:cursor-pointer'>{title}</h2>
-                <h3>{subtitle}</h3>
-
+            <div className='lg:p-10 p-8'>
+                <h2 className='text-c-green font-manjari lg:text-2xl text-xl font-extrabold hover:brightness-125 transition duration-150 ease-out hover:ease-in hover:cursor-pointer'>
+                  {title}
+                </h2>
+                <h3 className='text-xs lg:text-base'>{subtitle}</h3>
             </div>
         </div>
-
-
     </div>
   )
 }
